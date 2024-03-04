125af6aaf3e3504f200496ba762dc7c1832d3a11
johanpq <johanp.queiroz@gmail.com>
2024-03-04 17:39:22 -0300

    fix:  bug

--- questao4.c
+++ questao4.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
-void imprimeRomano(int numero) {
-    int valoresDecimais[] = {100, 90, 50, 40, 10, 9, 5, 4, 1};
-    char *algarismosRomanos[] = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    for (int i = 0; i < 9; i++) {
-        while (numero >= valoresDecimais[i]) {
-            printf("%s", algarismosRomanos[i]);
-            numero -= valoresDecimais[i];
+void converter_para_romano(int numero) {
+    int i;
+    const int valores_decimal[] = { 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    const char *valores_romanos[] = { "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    if (numero < 1 || numero > 100) {
+        printf("Número fora do intervalo permitido (1-100)\n");
+        return;
+    }
+
+    printf("%d = ", numero);
+
+    for (i = 0; i < 9; i++) {
+        while (numero >= valores_decimal[i]) {
+            printf("%s", valores_romanos[i]);
+            numero -= valores_decimal[i];
         }
     }
+    printf("\n");
 }
 
-int main(void) {
-    for (int i = 1; i <= 100; i++) {
-        printf("%d\t\t", i);
-        imprimeRomano(i);
-        printf("\n");
+int main() {
+    int i;
+    printf("Tabela de números decimais para algarismos romanos (1-100):\n");
+    for (i = 1; i <= 100; i++) {
+        converter_para_romano(i);
     }
-
     return 0;
 }
